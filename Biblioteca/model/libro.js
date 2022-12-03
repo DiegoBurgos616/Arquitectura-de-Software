a23225290752dd03d29accec4869468b106f5630
Diego Burgos <a20216388@alumnos.uady.mx>
2022-12-03 14:27:05 -0600

    Mostrar imagenes

--- Biblioteca/model/libro.js
+++ Biblioteca/model/libro.js
@@ -2,8 +2,8 @@ module.exports={
     obtener:function(conexion,funcion){
         conexion.query("SELECT * FROM libros",funcion);
     },
-    insertar:function(conexion,datos,funcion){
-        conexion.query("INSERT INTO libros (nombre,imagen) VALUES (?,?) ",[datos.nombre,datos.archivo],funcion);
+    insertar:function(conexion,datos,archivos,funcion){
+        conexion.query("INSERT INTO libros (nombre,imagen) VALUES (?,?) ",[datos.nombre,archivos.filename],funcion);
 
     }
 
--- Biblioteca/routes/libros.js
+++ Biblioteca/routes/libros.js
@@ -1,15 +1,33 @@
 var express = require('express');
-
 var router = express.Router();
-const librosController=require("../controllers/librosController");
+const librosController = require("../controllers/librosController");
 
+var multer = require("multer");
+var fecha = Date.now();
+
+
+//Esta funcion sirve para que en caso de que dos imagenes con el mismo nombre se suban
+//poder hacer una diferencia con la fecha y no se sobreescriban
+var rutaAlmacen = multer.diskStorage(
+    {
+        destination: function (request, file, callback) {
+            callback(null, "./public/images/");
+        },
+        filename: function (request, file, callback) {
+            console.log(file);
+            callback(null, fecha + "-" + file.originalname);
+        }
+    }
+);
+
+var cargar=multer({storage:rutaAlmacen});
 /* GET home page. */
 
 
-router.get("/",librosController.index);
-router.get("/crear",librosController.crear);
+router.get("/", librosController.index);
+router.get("/crear", librosController.crear);
 
-router.post("/",librosController.guardar);
+router.post("/", cargar.single("archivo"),librosController.guardar);
 
 
 module.exports = router;
